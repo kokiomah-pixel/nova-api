88848fdd6dda3a4d74016469d196c43b61b327f0
sharpenova <kokiomah@gmail.com>
2026-06-28 13:04:55 -0500

    feat(harness): export offline governance records

--- nova/harnesses/agent_prepared_financial_action_review/__init__.py
+++ nova/harnesses/agent_prepared_financial_action_review/__init__.py
@@ -1,4 +1,5 @@
 from .batch_replay import run_batch_replay, run_batch_replay_for_directory
+from .exporter import export_governance_records, safe_filename
 from .governance_record import build_governance_record
 from .reviewer import review_agent_prepared_action
 from .schema import AgentPreparedFinancialAction, ReviewOutput
@@ -8,6 +9,8 @@ __all__ = [
     "ReviewOutput",
     "build_governance_record",
     "review_agent_prepared_action",
+    "export_governance_records",
     "run_batch_replay",
     "run_batch_replay_for_directory",
+    "safe_filename",
 ]
