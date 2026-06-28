d0a7f3b4e40854f5a8c143c6e39f9fd531107d77
sharpenova <kokiomah@gmail.com>
2026-06-28 15:02:28 -0500

    feat(harness): package chronology ingestion candidates

--- nova/harnesses/agent_prepared_financial_action_review/__init__.py
+++ nova/harnesses/agent_prepared_financial_action_review/__init__.py
@@ -1,4 +1,5 @@
 from .batch_replay import run_batch_replay, run_batch_replay_for_directory
+from .chronology_package import build_chronology_ingestion_package
 from .exporter import export_governance_records, safe_filename
 from .governance_record import build_governance_record
 from .reviewer import review_agent_prepared_action
@@ -7,6 +8,7 @@ from .schema import AgentPreparedFinancialAction, ReviewOutput
 __all__ = [
     "AgentPreparedFinancialAction",
     "ReviewOutput",
+    "build_chronology_ingestion_package",
     "build_governance_record",
     "review_agent_prepared_action",
     "export_governance_records",
