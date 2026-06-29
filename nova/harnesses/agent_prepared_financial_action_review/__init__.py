7da5ddd4ab8b4e2186e9bc833785453bbfe037c3
sharpenova <kokiomah@gmail.com>
2026-06-28 22:36:48 -0500

    feat(harness): ledger chronology acceptance lifecycle

--- nova/harnesses/agent_prepared_financial_action_review/__init__.py
+++ nova/harnesses/agent_prepared_financial_action_review/__init__.py
@@ -1,5 +1,8 @@
 from .batch_replay import run_batch_replay, run_batch_replay_for_directory
 from .chronology_acceptance import create_manual_chronology_acceptance_decision
+from .chronology_acceptance_ledger import (
+    create_or_append_chronology_acceptance_ledger_entry,
+)
 from .chronology_movement_plan import create_accepted_records_manual_movement_plan
 from .chronology_package import build_chronology_ingestion_package
 from .exporter import export_governance_records, safe_filename
@@ -14,6 +17,7 @@ __all__ = [
     "build_governance_record",
     "create_accepted_records_manual_movement_plan",
     "create_manual_chronology_acceptance_decision",
+    "create_or_append_chronology_acceptance_ledger_entry",
     "review_agent_prepared_action",
     "export_governance_records",
     "run_batch_replay",
